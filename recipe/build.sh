68594e5a3dc8ed1aa299f421cb082504d7eb4acc
Isuru Fernando <isuruf@gmail.com>
2025-10-16 09:22:31 -0500

    fix cflags

--- recipe/build.sh
+++ recipe/build.sh
@@ -29,8 +29,19 @@ if [[ "$target_platform" == linux* ]]; then
   sed -i 's:@toolexeclibdir@:${libdir}:g' libffi.pc.in
 fi
 
+if [[ "$target_platform" == win-64 ]]; then
+  export CFLAGS="$CFLAGS --target=aarch64-pc-windows-msvc"
+elif [[ "$target_platform" == win-arm64 ]]; then
+  export CFLAGS="$CFLAGS --target=x86_64-pc-windows-msvc"
+fi
+
 ./configure "${configure_args[@]}" || { cat x86_64-pc-mingw32/config.log; exit 1;}
 if [[ "$target_platform" == win-64 ]]; then
+  pushd x86_64-pc-mingw64
+    patch_libtool
+    sed -i.bak 's/|-fuse-ld/|-Xclang|-fuse-ld/g' libtool
+  popd
+elif [[ "$target_platform" == win-arm64 ]]; then
   pushd x86_64-pc-mingw32
     patch_libtool
     sed -i.bak 's/|-fuse-ld/|-Xclang|-fuse-ld/g' libtool
